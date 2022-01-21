d13af14ccb48133b4356e7b9e8ba53a1007536ee
Patrycja Przybylska <patr.przybylska@gmail.com>
2022-01-21 19:27:08 +0100

    Poprawka

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -17,5 +17,7 @@ namespace WebApplication2.Data
         public DbSet<Product> Products{ get; set; }
         public DbSet<Employee> Employees{ get; set; }
         public DbSet<CartItem> Cart { get; set; }
+        public DbSet<Bon> Bony { get; set; }
+
     }
 }
